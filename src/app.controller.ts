08e9c66af11e4a2950d89fadef221ecf787a6af1
santi tan <pongsanti.tanvejsilp@gmail.com>
2020-01-21 12:30:50 +0700

    pug template

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
+  @Render('index')
+  root() {
+    return {
+      pageTitle: 'Hello world',
+      youAreUsingPug: true,
+      numbers: [
+        { id: 1, name: 'A' },
+        { id: 2, name: 'B' },
+        { id: 3, name: 'C' },
+      ],
+    };
+  }
+
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
   }
